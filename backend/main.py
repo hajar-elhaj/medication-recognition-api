6ab859d15b8fa91161baeadd26ac4c6edc3af809
Hajar <hajar.elhaj20@gmail.com>
2026-06-04 15:02:55 +0100

    Modifying the code (adding dosage and usage)

--- backend/main.py
+++ backend/main.py
@@ -2,26 +2,39 @@ from ocr import extract_text
 from db import get_connection
 from matcher import find_best_match
 
-#IMAGE PATH
-image_path = r"C:\Users\hajar\OneDrive\Bureau\medication-recognition-api\images\dolipran.jpg"
+# IMAGE PATH
+image_path = r"C:\Users\frita\OneDrive\Desktop\medication-recognition-api\images\dolipran.jpg"
 
-#OCR STEP
+# OCR STEP
 text = extract_text(image_path)
+
 print("\nOCR RESULT:")
 print(text)
 
-#CONNECT DB
+# CONNECT DB
 db = get_connection()
 cursor = db.cursor()
+cursor.execute("SELECT name, dosage, usage_description FROM medicines")
+rows = cursor.fetchall()
 
-cursor.execute("SELECT name FROM medicines")
-medicines = [row[0] for row in cursor.fetchall()]
+medicines = [row[0] for row in rows]
 
-print("\nDATABASE MEDICINES:")
-print(medicines)
-
-#MATCHING
 result = find_best_match(text, medicines)
 
+best_medicine = result[0]
+similarity = result[1]
+
+dosage = ""
+usage = ""
+
+for row in rows:
+    if row[0] == best_medicine:
+        dosage = row[1]
+        usage = row[2]
+        break
+
 print("\nFINAL RESULT:")
-print(result)
+print("Medicine:", best_medicine)
+print("Dosage:", dosage)
+print("Usage:", usage)
+print("Similarity:", similarity)
